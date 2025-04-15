ba853f06724b3a60a92bd6a3cffb70a363f9e33d
Cam Copland <cjcopland@gmail.com>
2025-04-15 11:30:44 +0100

    improved solution for readability 69

--- 69.py
+++ 69.py
@@ -7,20 +7,18 @@ class Solution(object):
         if x < 2:
             return x
 
-        min = 0
-        max = x
+        left, right = 1, x // 2
         
-        while True:
-            if max == min + 1:
-                return min
-            mid = (max + min) // 2
+        while left <= right:
+            mid = (right + left) // 2
             if mid * mid == x:
                 return mid
-            if mid * mid > x:
-                max = mid
-            if mid * mid < x:
-                min = mid
+            elif mid * mid < x:
+                left = mid + 1
+            else:
+                right = mid - 1
         
+        return right
 
 sol = Solution()
 print(sol.mySqrt(6))
