1875c18d6760d8dfc2369fe989996d3d91e4df51
Cam Copland <cjcopland@gmail.com>
2025-05-02 09:44:26 +0100

    first solution 171

---
+++ 171.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def titleToNumber(self, columnTitle):
+        """
+        :type columnTitle: str
+        :rtype: int
+        """
+        vals = [ord(c) - 64 for c in columnTitle]
+
+        val = 0
+        for i, n in enumerate(vals[::-1]):
+            val += n * 26**i
+        return val
