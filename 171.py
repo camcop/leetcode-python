2f7cb4fe8e353b2291d14821cb41fa944a97ba18
Cam Copland <cjcopland@gmail.com>
2025-05-02 09:44:52 +0100

    refactor to one line 171

--- 171.py
+++ 171.py
@@ -4,9 +4,11 @@ class Solution(object):
         :type columnTitle: str
         :rtype: int
         """
-        vals = [ord(c) - 64 for c in columnTitle]
+        # vals = [ord(c) - 64 for c in columnTitle]
 
-        val = 0
-        for i, n in enumerate(vals[::-1]):
-            val += n * 26**i
-        return val
+        # val = 0
+        # for i, n in enumerate(vals[::-1]):
+        #     val += n * 26**i
+        # return val
+
+        return sum([(ord(c) - 64) * 26**i for i, c in enumerate(columnTitle[::-1])])
